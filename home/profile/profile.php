9d942335f6ec92589b30c8bb4d2cb10545183f36
Jordan Tran <75463422+jordantran22@users.noreply.github.com>
2021-02-28 20:57:52 -0500

    Created Profile Card Design
    Created profile menu with contacts, friends, and followers

--- home/profile/profile.php
+++ home/profile/profile.php
@@ -1,6 +1,49 @@
       <div class="bg-white px-4 py-4 m-0 lg:ml-96 lg:mr-96 transition shadow-none md:shadow border-b border-6 " id="normalpost">
-sss
-	  </div>
+
+<div class="flex items-center justify-center">
+        <div class="bg-white w-1/3 mt-10 rounded-lg">
+            <div class="flex items-center justify-center pt-10">
+                <div class="bg-gradient-to-tr from-yellow-400 to-red-700 p-1 rounded-full">
+                    <a href="">
+                        <img src="https://media-exp1.licdn.com/dms/image/C5603AQFgqLPPPZg0zA/profile-displayphoto-shrink_800_800/0/1607055724779?e=1619654400&v=beta&t=KaDhnqApoqDEzAVHwKydAvjpCLH0xF6fcnX2ArTFK70"
+                             class="bg-white p-1 rounded-full w-32 " />
+                    </a>
+                </div>
+
+            </div>
+            <h1 class="flex items-center justify-center text-gray-800 font-semibold text-xl mt-5">Jordan Tran</h1>
+            <h1 class="flex items-center justify-center text-gray-500 text-sm">Detroit, Michigan</h1>
+            <h1 class="flex items-center justify-center text-gray-500 text-sm p-4 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </h1>
+
+            <div class="grid grid-cols-3 divide-x divide-yellow-500 text-center">
+                <div class="text-yellow-600 font-bold text-xl ">Contacts</div>
+                <div class="text-yellow-600 font-bold text-xl ">Friends</div>
+                <div class="text-yellow-600 font-bold text-xl ">Followers</div>
+            </div>
+            <div class="grid grid-cols-3 divide-x divide-yellow-500 text-center font-sans ">
+                <div class="font-bold text-xl">50</div>
+                <div class="font-bold text-xl">23</div>
+                <div class="font-bold text-xl">10</div>
+            </div>
+
+            <div class="py-3">
+            </div>
+
+
+            <div class="flex justify-center">
+                <button class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 p-6 py-4 " type="submit">Request</button>
+            </div>
+
+            <div class="py-3">
+            </div>
+        </div>
+
+   
+	
 	  
 	  
 	  
