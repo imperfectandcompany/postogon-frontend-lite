6df9439d2837e1305befa68831b95b38b766bc06
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-03-03 18:39:32 -0500

    small fixes
    pull this one @jordantran22

---
+++ home/post.php
@@ -0,0 +1,11 @@
+
+
+
+<div class="splide">
+	<div class="splide__track">
+		<ul class="splide__list">
+			<li class="splide__slide">			<?php include('slide1.php');?></li>
+			<li class="splide__slide">			<?php include('slide2.php');?></li>
+		</ul>
+	</div>
+</div>
---
+++ home/profile/profile.php
@@ -0,0 +1,74 @@
+      <div class="bg-white px-4 py-4 m-0 lg:ml-96 lg:mr-96 transition shadow-none md:shadow border-b border-6 " id="normalpost">
+
+<div class="flex items-center justify-center">
+        <div class="bg-white mt-10 rounded-lg">
+            <div class="flex items-center justify-center pt-10">
+                <div class="bg-gradient-to-tr from-yellow-400 to-red-700 p-1 rounded-full">
+                    <a href="">
+                        <img src="https://media-exp1.licdn.com/dms/image/C5603AQFgqLPPPZg0zA/profile-displayphoto-shrink_800_800/0/1607055724779?e=1619654400&v=beta&t=KaDhnqApoqDEzAVHwKydAvjpCLH0xF6fcnX2ArTFK70"
+                             class="bg-white p-1 rounded-full w-32 " />
+                    </a>
+                </div>
+
+            </div>
+            <h1 class="flex items-center justify-center text-gray-800 font-semibold text-xl mt-5">Jordan Tran</h1>
+            <h1 class="flex items-center justify-center text-gray-500 text-sm">Detroit, Michigan</h1>
+            <h1 class="flex items-center justify-center text-gray-500 text-sm p-4 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </h1>
+
+
+<div class="flex justify-center space-x-2">
+
+<div class="text-center">
+<div class="text-center text-yellow-600 uppercase font-bold text-lg">
+Contacts
+</div>
+<div class="text-lg font-semibold">
+50
+</div>
+</div>
+
+<div class="text-center">
+<div class="text-center text-yellow-600 uppercase font-bold text-lg">
+Friends
+</div>
+<div class="text-lg font-semibold">
+50
+</div>
+</div>
+
+<div class="text-center">
+<div class="text-center text-yellow-600 uppercase font-bold text-lg">
+Followers
+</div>
+<div class="text-lg font-semibold">
+50
+</div>
+</div>
+
+</div>
+
+
+
+
+            <div class="py-3">
+            </div>
+
+
+            <div class="flex justify-center">
+                <button class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 p-6 py-4 " type="submit">Request</button>
+            </div>
+
+            <div class="py-3">
+            </div>
+        </div>
+</div>
+ </div>
+	
+	  
+	  
+	  
+	 
