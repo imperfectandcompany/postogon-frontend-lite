3d91168e0cb6e7cf2d6c8f0072963361cf52179d
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-03-03 17:12:04 -0500

    made it responsive
    @jordantran22

--- home/messages/messages.php
+++ home/messages/messages.php
@@ -1,103 +1,130 @@
       <div class="bg-white px-4 py-4 m-0 lg:ml-96 lg:mr-96 transition shadow-none md:shadow border-b border-6 " id="normalpost">
 
- <div class="h-screen">
-        <div class="flex-grow h-screen bg-gray-100">
-            <div class="flex px-4 py-4 text-4xl font-bold">Messages</div>
-            <div class="p-3 ">
-                <input class="p-3 w-full bg-gray-200 rounded-lg focus:ring-2 focus-yellow-600" type="text" placeholder="Search For Contacts..." />
 
+        <div class="flex-grow">
+            <div class="flex py-4 text-4xl font-bold">Messages</div>
+			
+			<div class="flex flex-1 m-2 justify-between text-center">
+			<div>
+                <img class="h-16 w-16 rounded-full border-4 rounded-full select-none cursor-pointer" src="../../assets/img/profile pictures/avatar1.png" />
+                <p>Jason</p>
+			</div>
+			
+			<div>
+                <img class="h-16 w-16 rounded-full border-4 select-none cursor-pointer" rounded-full" src="../../assets/img/profile pictures/avatar2.png" />
+                <p>Allison</p>
+			</div>
+			
+			<div>
+                <img class="h-16 w-16 rounded-full border-4 select-none cursor-pointer" rounded-full border-4" src="../../assets/img/profile pictures/avatar3.png" />
+                <p>Will</p>
+			</div>
+			
+			<div>
+                <img class="h-16 w-16 rounded-full border-4 select-none cursor-pointer" rounded-full" src="../../assets/img/profile pictures/avatar4.png" />
+                <p>Lenny</p>
+			</div>
+			
+			<div>
+                <img class="h-16 w-16 rounded-full border-4 select-none cursor-pointer" rounded-full" src="../../assets/img/profile pictures/avatar5.png" />
+                <p>Sandy</p>
+			</div>
+			</div>			
+            <div class="p-3">
+                <input class="p-3 w-full bg-gray-200 hover:opacity-75 transition rounded-lg ring-inset focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:opacity-50" type="text" placeholder="Search For Contacts..." />
             </div>
 
-            <div class="flex justify-around">
-                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar1.png" />
-                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar2.png" />
-                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar3.png" />
-                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar4.png" />
-                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar5.png" />
 
 
-
-
-            </div>
-
-           
-
-            <div class="flex justify-around sm:text-xs md:text-lg lg:text-xl text-gray-500 pt-1  ">
-
-                <p class="">Jason</p>
-                <p class="">Allison</p>
-                <p class="">Will</p>
-                <p class="">Lenny</p>
-                <p class="">Sandy</p>
-            </div>
-
-
-
-
-            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
-
-
-                <div class="flex">
-                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar1.png" />
-                    <div class="font-bold text-xl p-3 ">Jason</div>
-
-                    <div class="text-base text-gray-500 px-3">
+            <div class="flex m-3 bg-white-300 p-2 rounded-lg shadow-md">
+					<div class="flex-none mr-3 mr-3 my-auto">
+					<div class="flex-none ">
+					<img class=" w-20 h-20  rounded-full border-4 select-none cursor-pointer" src="../../assets/img/profile pictures/avatar1.png" />
+					</div>
+					</div>
+					<div class="flex flex-col w-full whitespace-nowrap truncate">
+					<div class="flex">
+                    <div class="font-bold text-lg">Jason</div>
+					<div class="font-base text-sm text-gray-300 ml-auto ">3h</div>
+					</div>
+                    <div class="text-base text-gray-500">
                         It was nice meeting you the other night. I am looking forward to connecting on Postogon.
-                    </div>
-                </div>
+                    </div>					
+					<div class="text-xs text-right mt-2 text-gray-400 transition hover:text-gray-500">Yale '22, Studying Engineering 💻</div>	
+					</div>
+			</div>
 
-
-
-
-
-            </div>
-
-            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
-
-                <div class="flex">
-                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar2.png" />
-                    <div class="font-bold text-xl p-3 ">Allison</div>
-
-                    <div class="text-base text-gray-500 px-3">
-                        Hey Allison, how is it going?
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
-
-                <div class="flex">
-                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar3.png" />
-                    <div class="font-bold text-xl p-3 ">Will</div>
-
-                    <div class="text-base text-gray-500 px-3">
-                        Hey! Loved your post bro, keep it up.
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
-
-                <div class="flex">
-                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar4.png" />
-                    <div class="font-bold text-xl p-3 ">Lenny</div>
-
-                    <div class="text-base text-gray-500 px-3">
+            <div class="flex m-3 bg-white-300 p-2 rounded-lg shadow-md">
+					<div class="flex-none my-auto">
+					<div class="flex-none m-2 ">
+					<img class=" w-20 h-20  rounded-full border-4 select-none cursor-pointer" src="../../assets/img/profile pictures/avatar2.png" />
+					</div>
+					</div>
+					<div class="flex flex-col w-full whitespace-nowrap truncate">
+					<div class="flex">
+                    <div class="font-bold text-lg">Allison</div>
+					<div class="font-base text-sm text-gray-300 ml-auto ">3h</div>
+					</div>
+                    <div class="text-base text-gray-500">
+                        Hey, how is it going?
+                    </div>					
+					<div class="text-xs text-right mt-2 text-gray-400 transition hover:text-gray-500">Live life now, we can only learn from mistakes</div>	
+					</div>
+			</div>
+			
+            <div class="flex m-3 bg-white-300 p-2 rounded-lg shadow-md">
+					<div class="flex-none my-auto">
+					<div class="flex-none m-2 ">
+					<img class=" w-20 h-20  rounded-full border-4 select-none cursor-pointer" src="../../assets/img/profile pictures/avatar3.png" />
+					</div>
+					</div>
+					<div class="flex flex-col w-full whitespace-nowrap truncate">
+					<div class="flex">
+                    <div class="font-bold text-lg">Will</div>
+					<div class="font-base text-sm text-gray-300 ml-auto ">3h</div>
+					</div>
+                    <div class="text-base text-gray-500">
                         I sent all the videos and pictures last night into The Boys gallery.
-                    </div>
-                </div>
+                    </div>					
+					<div class="text-xs text-right mt-2 text-gray-400 transition hover:text-gray-500">Just a guy tryna make it outchea</div>	
+					</div>
+			</div>
+			
+            <div class="flex m-3 bg-white-300 p-2 rounded-lg shadow-md">
+					<div class="flex-none my-auto">
+					<div class="flex-none m-2 ">
+					<img class=" w-20 h-20  rounded-full border-4 select-none cursor-pointer" src="../../assets/img/profile pictures/avatar4.png" />
+					</div>
+					</div>
+					<div class="flex flex-col w-full whitespace-nowrap truncate">
+					<div class="flex">
+                    <div class="font-bold text-lg">Lenny</div>
+					<div class="font-base text-sm text-gray-300 ml-auto ">3h</div>
+					</div>
+                    <div class="text-base text-gray-500">
+                        Hey! Loved your post bro, keep it up.
+                    </div>	
+					<div class="text-xs text-right mt-2 text-gray-400 transition hover:text-gray-500">OSU Alumni - Mechanical Engineer - Music lover.</div>	
+					</div>					
 
-            </div>
+			</div>
 
-        
-
-
-
+            <div class="flex m-3 bg-white-300 p-2 rounded-lg shadow-md">
+					<div class="flex-none my-auto">
+					<div class="flex-none m-2 ">
+					<img class=" w-20 h-20  rounded-full border-4 select-none cursor-pointer" src="../../assets/img/profile pictures/avatar5.png" />
+					</div>
+					</div>
+					<div class="flex flex-col w-full whitespace-nowrap truncate">
+					<div class="flex">
+                    <div class="font-bold text-lg">Sandy</div>
+					<div class="font-base text-sm text-gray-300 ml-auto ">3h</div>
+					</div>
+                    <div class="text-base text-gray-500">
+                        Hey 😅, I know you we had plans for Saturday but I just realized I have exam this Monday? Raincheck 
+                    </div>					
+					<div class="text-xs text-right mt-2 text-gray-400 transition hover:text-gray-500">I'm still figuring stuff out. Sometimes I write stuff.</div>	
+					</div>
+			</div>	
         </div>
    
-	  
-	  
-
-        </div>
