1e10554374a1d66ea482d22b4689b44b65f28c83
Jordan Tran <75463422+jordantran22@users.noreply.github.com>
2021-03-03 12:12:22 -0500

    Messaging Page
    Created messaging user interface front-end design

---
+++ home/messages/messages.php
@@ -0,0 +1,103 @@
+      <div class="bg-white px-4 py-4 m-0 lg:ml-96 lg:mr-96 transition shadow-none md:shadow border-b border-6 " id="normalpost">
+
+ <div class="h-screen">
+        <div class="flex-grow h-screen bg-gray-100">
+            <div class="flex px-4 py-4 text-4xl font-bold">Messages</div>
+            <div class="p-3 ">
+                <input class="p-3 w-full bg-gray-200 rounded-lg focus:ring-2 focus-yellow-600" type="text" placeholder="Search For Contacts..." />
+
+            </div>
+
+            <div class="flex justify-around">
+                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar1.png" />
+                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar2.png" />
+                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar3.png" />
+                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar4.png" />
+                <img class="w-1/6 p-0.5 rounded-full bg-gradient-to-tr from-yellow-400 to-red-700" src="../../assets/img/profile pictures/avatar5.png" />
+
+
+
+
+            </div>
+
+           
+
+            <div class="flex justify-around sm:text-xs md:text-lg lg:text-xl text-gray-500 pt-1  ">
+
+                <p class="">Jason</p>
+                <p class="">Allison</p>
+                <p class="">Will</p>
+                <p class="">Lenny</p>
+                <p class="">Sandy</p>
+            </div>
+
+
+
+
+            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
+
+
+                <div class="flex">
+                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar1.png" />
+                    <div class="font-bold text-xl p-3 ">Jason</div>
+
+                    <div class="text-base text-gray-500 px-3">
+                        It was nice meeting you the other night. I am looking forward to connecting on Postogon.
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+
+            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
+
+                <div class="flex">
+                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar2.png" />
+                    <div class="font-bold text-xl p-3 ">Allison</div>
+
+                    <div class="text-base text-gray-500 px-3">
+                        Hey Allison, how is it going?
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
+
+                <div class="flex">
+                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar3.png" />
+                    <div class="font-bold text-xl p-3 ">Will</div>
+
+                    <div class="text-base text-gray-500 px-3">
+                        Hey! Loved your post bro, keep it up.
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="flex m-3 bg-white p-2 rounded-lg shadow-xl">
+
+                <div class="flex">
+                    <img class=" w-16 h-16  rounded-full" src="../../assets/img/profile pictures/avatar4.png" />
+                    <div class="font-bold text-xl p-3 ">Lenny</div>
+
+                    <div class="text-base text-gray-500 px-3">
+                        I sent all the videos and pictures last night into The Boys gallery.
+                    </div>
+                </div>
+
+            </div>
+
+        
+
+
+
+        </div>
+   
+	  
+	  
+
+        </div>
