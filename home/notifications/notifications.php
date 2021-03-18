dd19cc70d83e1b7374e534e8b25edea12cd95c43
Jordan Tran <75463422+jordantran22@users.noreply.github.com>
2021-03-18 11:28:08 -0400

    Notifications Page COMPLETED
    - Created notifications page UI
    - Fully responsive for all mobile devices
    - Created a new notifications folder within the home folder
    - Fixed some errors and added comments to the code

---
+++ home/notifications/notifications.php
@@ -0,0 +1,156 @@
+      <div class="bg-white px-4 py-4 m-0 lg:ml-96 lg:mr-96 transition shadow-none md:shadow border-b border-6 " id="normalpost">
+
+<body class="md:bg-gray-100">
+
+
+<div class="py-2 md:p-20">
+
+<div class="md:bg-white p-4 rounded-lg shadow-2xl">
+
+
+<!-- Link to REQUEST PAGE to accept or decline -->
+<a href="">
+<div class= "flex justify-between m-3">
+		
+		
+		<div class="font-semibold text-lg md:text-2xl font-bold lg:text-4xl ">Requests </div>
+		
+		
+		<div class="flex">
+		
+		<div class="w-4 h-4 m-2 mr-3 md:w-6 h-6 lg:w-8 h-8">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+</svg>
+
+		</div>
+		
+		
+		<div class="text-lg font-light md:text-2xl font-light lg:text-4xl ">
+		29
+		</div>
+		
+		<div class="w-5 h-5 m-1 md:w-7 h-7 lg:w-9 h-9">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+</svg>
+		</div>
+		
+		
+		</div>
+		
+</div>
+</a>
+
+
+		<hr>
+
+
+		<!-- Switch Notifications Category -->
+	<div class="flex justify-evenly p-2 md:text-lg lg:text-2xl p-3">
+		<button class="bg-gray-800 text-white font-semibold rounded-lg p-1">
+		All Notifications
+		</button>
+
+		<button class="bg-gray-200 text-gray-800 font-semibold rounded-lg p-2">
+			Recent 
+			</button>
+
+	</div>
+
+
+
+				<!-- User Notifications Start Here -->
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1 flex-shrink-0" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Sara Salah</span> replied to your comment, <span class="font-bold text-blue-500" href="#">"Happy Birthday Sara!!"</span>  2m
+                    </p>
+                </a>
+				
+					
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Slick Net</span> is now following you. 45m
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Jane Doe</span> Like Your reply <span class="font-bold text-blue-500" href="#">"Nice bro"</span> 1h
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=398&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Abigail Bennett</span> is now a contact.   3h
+                    </p>
+                </a>
+				
+				<hr>
+				
+				
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Janet Peterson </span> has joined the circle <span class="font-bold text-blue-500" href="#">Family!</span> 3h
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Brad Walsh</span> has requested to be a contact. 6h
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Saul Leonard</span> tagged you on a <span class="font-bold text-blue-500" href="#">post</span>. 14h
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1158&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Nicholas Henry</span> liked your post <span class="font-bold text-blue-500" href="#">"Hey everyone, thank you all..."</span> 18h
+                    </p>
+                </a>
+				
+				<hr>
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Kristy White</span> is now a contact. 1d ago
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1535295972055-1c762f4483e5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Lucy Ramos</span> is now following you . 2d ago
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Fred Griffin</span> messaged you. 2d ago
+                    </p>
+                </a>
+                <a href="#" class="flex items-center px-4 py-3 hover:bg-gray-100 -mx-2">
+                    <img class="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1484863137850-59afcfe05386?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="avatar">
+                    <p class="text-gray-600 text-sm mx-2 md:text-lg lg:text-2xl">
+                        <span class="font-bold" href="#">Linda Mack</span> is now following you . 3h
+                    </p>
+                </a>
+				
+				<!--User Notifications END HERE -->
+				
+				
+            </div>
+		</div>
+
+
+
+</body>
+	  
+	  
+	 
